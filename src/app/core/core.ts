4002432f7d44c430cf4b9857b21c317950f2f368
DimaSavchenko <ddimasauchanka@gmail.com>
2016-09-01 14:00:12 +0300

    feat(structure): add new navbar component

--- src/app/core/core.ts
+++ src/app/core/core.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
 import {Sidebar} from './sidebar';
+import {Navbar} from './navbar';
 
 @Component({
   selector: '[core]',
   templateUrl: './core.template.html',
   styles: [require('./core.style.scss')],
-  directives: [Sidebar]
+  directives: [Sidebar, Navbar]
 })
 export class Core {
 
