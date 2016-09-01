49c5b4ac66cf200927d1350918c2dd757ebc2e06
DimaSavchenko <ddimasauchanka@gmail.com>
2016-09-01 13:58:10 +0300

    feat(structure): add new sidebar component

--- src/app/core/core.ts
+++ src/app/core/core.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
+import {Sidebar} from './sidebar';
+
 @Component({
-  selector: 'core',
-  template: `
-    <div>
-      <h1>Core page</h1>
-    </div>
-  `
+  selector: '[core]',
+  templateUrl: './core.template.html',
+  styles: [require('./core.style.scss')],
+  directives: [Sidebar]
 })
 export class Core {
 
---
+++ src/app/core/sidebar/sidebar.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '[sidebar]',
+  templateUrl: './sidebar.template.html',
+  styles: [require('./sidebar.style.scss')]
+})
+export class Sidebar {
+
+}
