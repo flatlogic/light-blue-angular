2a38a736fe8f63fed21238e5b556effcee8a5c95
DimaSavchenko <ddimasauchanka@gmail.com>
2016-10-12 15:39:20 +0300

    fix(structure): remove components from full LB version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,8 +9,6 @@ export const ROUTES: Routes = [{
 }, {
   path: 'login', loadChildren: () => System.import('./login/login.module')
 }, {
-  path: 'landing', loadChildren: () => System.import('./landing/landing.module')
-}, {
   path: 'error', component: ErrorComponent
 }, {
   path: '**',    component: ErrorComponent
--- src/app/layout/layout.routes.ts
+++ src/app/layout/layout.routes.ts
@@ -5,13 +5,7 @@ const routes: Routes = [
   { path: '', component: Layout, children: [
     { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
     { path: 'dashboard', loadChildren: () => System.import('../dashboard/dashboard.module') },
-    { path: 'form', loadChildren: () => System.import('../form/form.module') },
-    { path: 'statistics', loadChildren: () => System.import('../statistics/statistics.module') },
-    { path: 'ui', loadChildren: () => System.import('../ui/ui.module') },
-    { path: 'components', loadChildren: () => System.import('../components/components.module') },
-    { path: 'tables', loadChildren: () => System.import('../tables/tables.module') },
-    { path: 'widgets', loadChildren: () => System.import('../widgets/widgets.module') },
-    { path: 'special', loadChildren: () => System.import('../special/special.module') }
+    { path: 'another', loadChildren: () => System.import('../another/another.module') }
   ]}
 ];
 
