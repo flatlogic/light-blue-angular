f454c82311430f18344e0a467d5686556dbdf9e5
DimaSavchenko <ddimasauchanka@gmail.com>
2016-10-19 17:44:46 +0300

    fit(chat): fix slimscroll, fix adding new  message

--- src/app/dashboard/chat/chat.component.ts
+++ src/app/dashboard/chat/chat.component.ts
@@ -1,12 +1,12 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
+declare var jQuery: any;
 @Component({
   selector: '[chat]',
   templateUrl: './chat.template.html'
 })
 export class Chat {
   _messages;
-  message;
   @Input()
   set messages(messages: Array<any>) {
     this._messages = messages || '<no message set>';
@@ -14,12 +14,41 @@ export class Chat {
 
   get name() { return this._messages; }
 
-  addMessage(message) {
-    this._messages.push({
-      'time': 'just now',
-      'sender': 'Tikhon Laninga',
-      'text': `${message}`,
-      'image': '../../../assets/img/2.png'
-    });
+  doneTyping($event) {
+    if($event.which === 13) {
+      this.pushMessage($event.target.value);
+      $event.target.value = null;
+      this.scrollDown();
+    }
+  }
+
+  addMessage(inputEl : HTMLInputElement) {
+    this.pushMessage(inputEl.value);
+    inputEl.value = null;
+    this.scrollDown();
+  }
+
+  private pushMessage(message):void {
+      this._messages.push({
+          'time': 'just now',
+          'sender': 'Tikhon Laninga',
+          'text': `${message}`,
+          'image': '../../../assets/img/2.png'
+      });
+  }
+
+  private scrollDown() {
+    setTimeout(()=> {
+        jQuery(".chat-messages").slimscroll({scrollBy: '500px'});
+    }, 10);
+  }
+
+  ngOnInit(): void {
+     jQuery(".chat-messages").slimscroll({
+        height: '240px',
+        size: '5px',
+        alwaysVisible: true,
+        railVisible: true
+     });
   }
 }
