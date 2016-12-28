c7e98d6c92a7549862e5ca2a80e99b09d274bb75
smartapant <smartapant@gmail.com>
2016-12-28 12:53:34 +0300

    feat(form elements page): appended inputs widget

--- src/app/form/elements/elements.component.ts
+++ src/app/form/elements/elements.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import * as data from '../account/account.data';
+
+declare var jQuery: any;
 
 @Component({
   selector: '[elements]',
   templateUrl: './elements.template.html',
-  styleUrls: [ './elements.style.scss' ]
+  styleUrls: [ './elements.style.scss' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class Elements {
   getSelect2CountryList(): [string] {
     return data.select2CountryData;
+  }
+
+  ngOnInit(): void {
+    jQuery('.selectpicker').selectpicker();
   }
 }
