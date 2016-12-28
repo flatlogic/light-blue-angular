7dfaa214223f5698a165e1d4c7be3f3b3f1bb72f
smartapant <smartapant@gmail.com>
2016-12-29 01:01:16 +0300

    feat(form elements page): input masks widget

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -11,6 +11,7 @@ import 'bootstrap-select/dist/js/bootstrap-select.js';
 import 'parsleyjs';
 import 'twitter-bootstrap-wizard/jquery.bootstrap.wizard.js';
 import 'bootstrap-colorpicker';
+import 'jasny-bootstrap/js/inputmask.js';
 import 'ng2-datetime/src/vendor/bootstrap-datepicker/bootstrap-datepicker.min.js';
 import 'ng2-datetime/src/vendor/bootstrap-timepicker/bootstrap-timepicker.min.js';
 
