169c8b8f75add1267e981a78755e08c9c2b58f19
smartapant <smartapant@gmail.com>
2017-01-06 18:27:31 +0300

    feat(nav tabs): some initial styles

--- src/app/ui/tabs/tabs.component.ts
+++ src/app/ui/tabs/tabs.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: '[tabs]',
   templateUrl: './tabs.template.html',
-  styleUrls: [ './tabs.style.scss' ]
+  styleUrls: [ './tabs.style.scss' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class Tabs {
 
