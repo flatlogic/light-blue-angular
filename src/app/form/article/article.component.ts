4013f52a0bdb97c85fdb029ebc71b3b46e354da3
Andrei Hrabouski <smartapant@gmail.com>
2017-01-19 16:45:16 +0300

    Form article (#31)
    * feat(form article page): article widget style
    
    * (form article): some more styles
    
    * feat(form article): markdown editor
    
    * feat(form article): remove useless parts for now
    
    * feat(form article): datepickers
    
    * fix(form article): some styles
    
    * feat(form article): title validation
    
    * feat(form-article): md editor layout
    
    * feat(form article): style md toolbar
    
    * fix(forms-article): fix ng2-datetime displaying on wide screed

--- src/app/form/article/article.component.ts
+++ src/app/form/article/article.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 declare var jQuery: any;
 
 @Component({
   selector: '[article]',
   templateUrl: './article.template.html',
-  styleUrls: [ './article.style.scss' ]
+  styleUrls: [ './article.style.scss' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class Article {
+  publishDate: Date;
+
   ngOnInit(): void {
     jQuery('.selectpicker').selectpicker();
+    jQuery('#article-form').parsley({
+      errorsContainer:  function ( elem, isRadioOrCheckbox ) {
+        return jQuery(elem.$element).closest('.form-group').children('label');
+      }
+    });
+    jQuery('#markdown-editor').markdown();
   }
 }
