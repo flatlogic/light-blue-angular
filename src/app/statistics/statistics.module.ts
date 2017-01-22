4263e563aa379b3c93dd472456e08a8e06239f85
smartapant <smartapant@gmail.com>
2017-01-22 18:07:39 +0300

    feat(charts): flot chart

--- src/app/statistics/statistics.module.ts
+++ src/app/statistics/statistics.module.ts
@@ -4,14 +4,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import {RickshawChartModule} from '../components/rickshaw/rickshaw.module';
+import { JqSparklineModule } from '../components/sparkline/sparkline.module';
+import { FlotChartModule } from '../components/flot/flot.module';
 
 import {Stats} from './stats/stats.component'
 import {Charts} from './charts/charts.component'
+import {FlotChartAnimator} from  './charts/flot-chart-animator/flot-chart-animator.directive';
 import {Realtime} from './realtime/realtime.component'
 
 import 'jquery-ui';
 import 'jquery-ui/ui/widgets/sortable.js';
 import 'jquery-ui/ui/widgets/resizable.js';
+import 'easy-pie-chart/dist/jquery.easypiechart.js';
 
 console.log('`Detail` bundle loaded asynchronously');
 // async components must be named routes for WebpackAsyncRoute
@@ -27,13 +31,16 @@ export const routes = [
     // Components / Directives/ Pipes
     Stats,
     Charts,
-    Realtime
+    Realtime,
+    FlotChartAnimator
   ],
   imports: [
     CommonModule,
     FormsModule,
     RickshawChartModule,
     RouterModule.forChild(routes),
+    JqSparklineModule,
+    FlotChartModule
   ]
 })
 export default class StatisticsModule {
