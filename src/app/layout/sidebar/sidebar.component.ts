dfcfa2e3d733c470312bc5f4980f8cae8e84449b
shoudaos <shoudaos92@gmail.com>
2017-01-24 16:29:46 +0300

    fix(mobile) fix mobile sidebar height

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Renderer } from '@angular/core';
 
 @Component({
   selector: '[sidebar]',
@@ -6,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class Sidebar {
 
+  constructor(private renderer: Renderer) {
+  }
+
+  setSidebarHeight(event) {
+    let sidebar = document.getElementById('side-nav');
+    if (window.innerWidth < 768){
+      let sidebarMarginTop = parseInt(window.getComputedStyle(sidebar).marginTop);
+      let sidebarMarginBottom = parseInt(window.getComputedStyle(sidebar).marginBottom);
+      let sidebarHeight = sidebar.offsetHeight + sidebarMarginTop + sidebarMarginBottom;
+      let closestAccordionGroup = event.target.closest('.accordion-group');
+      let submenuHeight = 0;
+      let submenuItems = closestAccordionGroup.querySelectorAll('ul > li');
+      submenuItems.forEach(() => {
+        submenuHeight += 26;
+      });
+      let expandedMenu = closestAccordionGroup.querySelector('.accordion-body').getAttribute('aria-expanded');
+      if(expandedMenu === 'false') {
+        sidebarHeight += submenuHeight;
+      } else {
+        sidebarHeight -= submenuHeight;
+      }
+      this.renderer.setElementStyle(document.querySelector('.content'), 'margin-top', sidebarHeight + 'px');
+    }
+  }
 }
