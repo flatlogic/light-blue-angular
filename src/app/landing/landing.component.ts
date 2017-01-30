70b1ce530184f674c50e459b7187c0e593b17199
shoudaos <shoudaos92@gmail.com>
2017-01-30 12:40:44 +0300

    fix(landing): fix #60 issue

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+declare var jQuery: any;
 
 @Component({
   selector: 'landing',
@@ -6,5 +9,23 @@ import { Component } from '@angular/core';
   styleUrls: [ './landing.style.scss' ]
 })
 export class Landing {
+  constructor( private route: ActivatedRoute ) {
 
+  }
+
+  onAnchorClick ( ) {
+    this.route.fragment.subscribe( f => {
+      let element: any = document.querySelector( '#' + f );
+      if ( element ) {
+        element.scrollIntoView(element)
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    jQuery(".back-to-top").on('click', function() {
+      jQuery("html, body").animate({ scrollTop: 0 }, "fast");
+      return false;
+    });
+  }
 }
