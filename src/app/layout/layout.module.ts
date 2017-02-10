167652ff466dc2da3a0c943688b577bf72416dca
DimaSavchenko <dimasauchanka@gmail.com>
2017-02-10 16:32:53 +0300

    feat: add directive for href='#'

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -8,13 +8,15 @@ import { ROUTES } from './layout.routes';
 import { Layout } from './layout.component';
 import { Sidebar } from './sidebar/sidebar.component';
 import { Navbar } from './navbar/navbar.component';
+import {EmptyLinkModule} from "./directives/empty-link/empty-link.module";
 
 @NgModule({
   imports: [
     CommonModule,
     ROUTES,
     ButtonsModule,
-    DropdownModule
+    DropdownModule,
+    EmptyLinkModule
   ],
   declarations: [Layout, Sidebar, Navbar]
 })
