928cfe9ec2f3d2444ae65a07b5536f09f218a4ad
shoudaos <shoudaos92@gmail.com>
2017-02-10 18:00:12 +0300

    bug(layout): fix collapsing menu on load, update node-sass

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
   selector: '[sidebar]',
   templateUrl: './sidebar.template.html'
 })
 export class Sidebar {
+  sidebarMenu: any = 0;
 
+  constructor(private el: ElementRef) {
+  }
+
+  ngAfterViewInit() {
+    this.sidebarMenu = this.el.nativeElement.querySelector('#side-nav');
+    if (window.innerWidth > 768) {
+      setTimeout(() => {
+        jQuery(this.sidebarMenu).find('.accordion-group.active .accordion-body').collapse('show');
+      });
+    }
+  }
 }
