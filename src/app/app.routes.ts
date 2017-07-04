621738959443bd388557a43991a96dcf3fce86f1
shoudaos <shoudaos92@gmail.com>
2017-07-04 15:25:03 +0300

    update libs, merge with angular-starter, fix bugs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 
 export const ROUTES: Routes = [{
-  path: '', redirectTo: 'app', pathMatch: 'full'
-}, {
-  path: 'app',   loadChildren: './layout/layout.module#LayoutModule'
-}, {
-  path: 'login', loadChildren: './login/login.module#LoginModule'
-}, {
-  path: 'error', loadChildren: './error/error.module#ErrorModule'
-}, {
-  path: '**',    loadChildren: './error/error.module#ErrorModule'
-}
+   path: '', redirectTo: 'app', pathMatch: 'full'
+  },
+  {
+    path: 'app',   loadChildren: './layout/layout.module#LayoutModule'
+  },
+  {
+    path: 'login', loadChildren: './login/login.module#LoginModule'
+  },
+  {
+    path: 'error', loadChildren: './error/error.module#ErrorModule'
+  },
+  {
+    path: '**',    loadChildren: './error/error.module#ErrorModule'
+  }
 ];
