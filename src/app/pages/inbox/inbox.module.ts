c4919bfaaa651f52e44146bfadf2108529ff23b9
Alexander Kaminski <a.kaminski@flatlogic.com>
2020-01-17 14:11:52 +0300

    refactor(mail-list): remove jquery
    remove jquery

--- src/app/pages/inbox/inbox.module.ts
+++ src/app/pages/inbox/inbox.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { InboxComponent } from './inbox.component';
@@ -17,6 +17,7 @@ export const routes = [
 @NgModule({
     imports: [
         FormsModule,
+        ReactiveFormsModule,
         CommonModule,
         RouterModule.forChild(routes) ],
     declarations: [
