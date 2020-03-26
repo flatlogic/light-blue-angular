ca031c7e5c753c48b3c0b046c009dfa388bdd818
Alexander Kaminski <a.kaminski@flatlogic.com>
2020-03-26 12:17:43 +0300

    refactor: use widgster component
    use widgster component inside package module

--- src/app/pages/package/package.module.ts
+++ src/app/pages/package/package.module.ts
@@ -4,6 +4,7 @@ import { RouterModule } from '@angular/router';
 
 import { Package } from './package.component';
 import { WidgetModule } from '../../layout/widget/widget.module';
+import { WidgsterModule } from '../../components/widgster/widgster.module';
 
 export const routes = [
   { path: '', component: Package, pathMatch: 'full' }
@@ -17,6 +18,7 @@ export const routes = [
     CommonModule,
     WidgetModule,
     RouterModule.forChild(routes),
+    WidgsterModule
   ]
 })
 export class PackageModule {
