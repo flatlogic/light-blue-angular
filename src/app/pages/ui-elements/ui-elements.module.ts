55c5d68b9125ecef547cdeaa1e62de4b54677941
Alexander <a.kaminski@flatlogic.com>
2020-03-31 11:18:09 +0300

    build: remove jquery.nestable, napa

--- src/app/pages/ui-elements/ui-elements.module.ts
+++ src/app/pages/ui-elements/ui-elements.module.ts
@@ -1,5 +1,3 @@
-import 'jquery.nestable/jquery.nestable.js';
-
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
